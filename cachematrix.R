461608547d69a3360ef7c931a9960cb1936dd090
Tan Yee Hern <xztanyh@gmail.com>
2015-02-19 15:27:46 +0800

    TYH R Programming rg-011 Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,37 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(m = matrix()) {
+  I<-NULL
+  set <- function(n){
+    m <<- n           # set the value of the matrix
+    I <<- NULL
+  }
+  get <- function(){
+    m                 # get the value of the matrix
+  }
+  setInverse <- function(inverse){
+    I <<- inverse     # set the value of the inverse
+  }
+  getInverse <- function(){
+    I                 # get the value of the inverse
+  }
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+  
         ## Return a matrix that is the inverse of 'x'
+  
+  I <- m$getInverse()      # Attempt to check cache
+  if(!is.null(I) && identical(solve(m),I)) {
+    message("getting cached data")
+    return(I)              # Return Inverse from cache if exists
+  }
+  data <- m$get()          
+  I <- solve(data)         # Compute Inverse if no cache data
+  m$setInverse(I)
+  I
 }
